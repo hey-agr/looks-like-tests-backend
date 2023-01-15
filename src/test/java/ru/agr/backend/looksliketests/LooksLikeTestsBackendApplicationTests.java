f1453d0633a801c6f0f2d5906ff15afd0f2ccc9f
agr <AGRabadanov@gmail.com>
2023-01-15 01:59:45 +0100

    refactor: removed unused class

--- src/test/java/ru/agr/backend/looksliketests/LooksLikeTestsBackendApplicationTests.java
+++ src/test/java/ru/agr/backend/looksliketests/LooksLikeTestsBackendApplicationTests.java
@@ -2,15 +2,21 @@ package ru.agr.backend.looksliketests;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import ru.agr.backend.looksliketests.utils.SpringPostgresIntegrationTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
 @SpringPostgresIntegrationTest
 class LooksLikeTestsBackendApplicationTests {
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
         log.info("LooksLikeTests backend successfully started!");
+        assertNotNull(context.getId());
     }
 }
